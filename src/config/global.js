57440f680bd6fb637ad7d9687d1f283fa2da80ec
Leyson Castaño <leysofcp@hotmail.com>
2025-04-19 10:46:44 -0500

    Update global.js

--- src/config/global.js
+++ src/config/global.js
@@ -206,13 +206,128 @@ export default {
   ],
   glosario: [
     {
-      termino: 'Término',
-      significado: 'Definición',
+      termino: 'Cartera de clientes',
+      significado:
+        'Registro que permite ordenar y clasificar a los clientes de una empresa, teniendo en cuenta entre otros aspectos, la ubicación, las facturas de venta, la cuantía y los vencimientos.',
+    },
+    {
+      termino: 'Deterioro de cartera',
+      significado:
+        'Reconocimiento anticipado de la probable pérdida del valor de las cuentas por cobrar a clientes por el no pago de la misma.',
+    },
+    {
+      termino: 'Devengo',
+      significado:
+        'Decrementos en los beneficios económicos, producidos a lo largo del período contable, en forma de salidas o disminuciones del valor de los activos, o bien por la generación o aumento de los pasivos que dan como resultado decrementos en el patrimonio.',
+    },
+    {
+      termino: 'Gastos',
+      significado:
+        'en software de diseño, son líneas que se crean y permiten, expandir, modificar, al tamaño que sea sin pixelarse porque se unen por coordenadas matemáticas del programa y no se generan por números fijos.',
+    },
+    {
+      termino: 'Ingresos',
+      significado:
+        'Incrementos en los beneficios económicos, producidos a lo largo del periodo contable, en forma de entradas o incrementos de valor de los activos, o bien como decrementos de los pasivos, que dan como resultado aumentos del patrimonio',
+    },
+    {
+      termino: 'Instrumentos financieros',
+      significado:
+        'Contrato que da lugar a un activo financiero en una empresa y simultáneamente a un pasivo financiero o a un instrumento de patrimonio en otra empresa. Activos Financieros: efectivo, cartera de clientes, inversiones, entre otros. Pasivos Financieros: Proveedores, obligaciones emitidas.',
+    },
+    {
+      termino: 'Inventarios para la venta',
+      significado:
+        'Activos, bienes corporales, destinados para la venta en la operación de comercialización, al igual que aquellos insumos y materiales utilizados en el proceso productivo para las empresas industriales.',
+    },
+    {
+      termino: 'kárdex',
+      significado:
+        'Conocido como auxiliar de inventarios o almacén, es un documento que por cada referencia de inventario muestra cronológicamente sus movimientos de entradas, salidas y saldos, tanto de unidades como de costos unitarios y totales.',
+    },
+    {
+      termino: 'Proveedores',
+      significado:
+        'Persona o una empresa que abastece los bienes o suministros, requeridos para la transformación y venta o que directamente se compran para su venta.',
+    },
+    {
+      termino: 'Valuación de inventarios',
+      significado:
+        'Asignación de valor a los inventarios destinados para la venta con el fin de hallar tanto el costo de venta como el costo de existencia, utilizando un método de valuación como promedio ponderado o primeras en entrar y primeras en salir',
+    },
+    {
+      termino: 'Venta',
+      significado:
+        'Operación que se caracteriza por la entrega del bien al cliente, transfiriéndole beneficios y riesgos, que generan ingresos para la empresa.',
     },
   ],
   referencias: [
     {
-      referencia: '',
+      referencia:
+        'Actualícese (2018) Decreto 2483 de 2018: Anexo Técnico Compilatorio No.2 de las Normas Internacionales de Información Financiera NIIF para Pymes Grupo 2: Secciones 2, 5, 11, 13 y 27 ',
+      link:
+        'https://cdn.actualicese.com/normatividad/2018/Decretos/Anexo-2-D2483-18.pdf',
+    },
+    {
+      referencia:
+        'Angulo, U. (2018). Contabilidad financiera, correlacionado con NIIF. Ediciones de la U.',
+      link: '',
+    },
+    {
+      referencia:
+        'Coral D., y Guidiño D., y Henao H. (2014). Contabilidad: Plus. Mc. Graw Hill.',
+    },
+    {
+      referencia:
+        'Cuellar D., Vargas R., y Castro L. (2012). Contabilidad: un enfoque práctico. Alfaomega.  ',
+      link: '',
+    },
+    {
+      referencia:
+        'Cuaspa, C. (2013). Normas internacionales de información financiera convergencia a Colombia y aplicación a empresas de salud. Ediciones Universidad Nacional de Colombia.',
+    },
+    {
+      referencia:
+        'Estatuto Tributario Nacional (2019) Deducción de deudas de dudoso o difícil cobro: Artículo 145 del Estatuto Tributario',
+      link: 'https://estatuto.co/?e=1126',
+    },
+    {
+      referencia: 'Gerencie (16 de diciembre de 2020). Provisión de Cartera',
+      link: 'https://www.gerencie.com/provision-de-cartera.html',
+    },
+    {
+      referencia:
+        'Ministerio de Industria Comercio y Turismo (2016) Norma Internacional de Contabilidad 2: Inventarios',
+      link:
+        'http://www.aplicaciones-mcit.gov.co/adjuntos/niif/9%20ES_RedBV2016_IAS02_PartA.pdf',
+    },
+    {
+      referencia:
+        'Ministerio de Industria Comercio y Turismo (2016) Norma Internacional de Contabilidad 8: Políticas Contables  ',
+      link:
+        'http://www.aplicaciones-mcit.gov.co/adjuntos/niif/11%20ES_RedBV2016_IAS08_PartA.pdf',
+    },
+    {
+      referencia:
+        'Ministerio de Industria Comercio y Turismo (2016) Norma Internacional de Información Financiera 7: Instrumentos Financieros.',
+      link:
+        'http://www.aplicaciones-mcit.gov.co/adjuntos/niif/52%20ES_RedBV2016_IFRS07_PartA.pdf',
+    },
+    {
+      referencia:
+        'Ministerio de Industria Comercio y Turismo (2016) Norma Internacional de Información Financiera 9: Instrumentos Financieros.',
+      link:
+        'http://www.aplicaciones-mcit.gov.co/adjuntos/niif/54%20ES_RedBV2016_IFRS09_PartA.pdf',
+    },
+    {
+      referencia:
+        'Ministerio de Industria Comercio y Turismo (2016) Norma Internacional de Información Financiera 15: Ingresos de Actividades ordinarias.',
+      link:
+        'http://www.aplicaciones-mcit.gov.co/adjuntos/niif/60%20ES_RedBV2016_IFRS15_PartA.pdf',
+    },
+    {
+      referencia:
+        'Sinisterra V., Polanco L., y Henao H. (2011). Contabilidad: sistema de información para organizaciones. Mc Graw Hill.',
       link: '',
     },
   ],
